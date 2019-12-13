1cedc11e1cbc765a7bf12a96672cdd0718b865c9
cdsalty <soltiscd@gmail.com>
2019-12-13 15:53:42 -0500

    project complete but task overlooked the lifecycles, stay tuned

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
 
+import "./Card.css";
+
 class Card extends Component {
+  constructor(props) {
+    super(props);
+    let angle = Math.random() * 90 - 45;
+    let xPos = Math.random() * 40 - 20;
+    let yPos = Math.random() * 40 - 20;
+    // let transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`; // this worked presiously but not that it's inside the constructor, it will always need " T H I S "
+    this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+    // will pass in transformn as a style in the return
+  }
   render() {
+    // transform: translate(10px, 20px)
+    // ISSUE: Doing it this way will cause the entire deck to change angles render; instead, move to constructor
+    // let angle = Math.random() * 90 - 45;
+    // let xPos = Math.random() * 40 - 20;
+    // let yPos = Math.random() * 40 - 20;
+    // let transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+    // // will pass in transformn as a style in the return
     return (
-      <img className="Card" src={this.props.image} alt={this.props.name} />
+      <img
+        style={{ transform: this._transform }}
+        className="Card"
+        src={this.props.image}
+        alt={this.props.name}
+      />
     );
   }
 }
@@ -16,4 +39,6 @@ export default Card;
 - to display the cards, we need to map through each card that is drawn,
 - then render that card's name, image and alt
 
+
+need to iterate over the drawn cards within the state, create a new card component for each one. 
 */
--- src/Deck.js
+++ src/Deck.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
+import Card from "./Card";
 import axios from "axios";
 
+import "./Deck.css";
 const API_BASE_URL = "https://www.deckofcardsapi.com/api/deck";
 // for card: https://deckofcardsapi.com/api/deck/${deck_id}/draw/
 
 class Deck extends Component {
   constructor(props) {
     super(props);
-    this.state = { deck: null };
+    this.state = { deck: null, drawn: [] };
     this.getCard = this.getCard.bind(this);
   }
 
@@ -17,17 +19,45 @@ class Deck extends Component {
     this.setState({ deck: deck.data });
   }
   async getCard() {
-    let id = this.state.deck.deck_id;
-    let cardUrl = `${API_BASE_URL}/${id}/draw/`;
-    let cardRes = await axios.get(cardUrl);
-    // console.log(cardRes); working
-    console.log(cardRes.data);
+    let deck_id = this.state.deck.deck_id;
+    try {
+      let cardUrl = `${API_BASE_URL}/${deck_id}/draw/`;
+      let cardRes = await axios.get(cardUrl);
+      // console.log(cardRes); working
+      // check what the card response looks like to prevent an error when running out of cards
+      if (!cardRes.data.success) {
+        //  if(cardRes.data.remaining === 0 will still leave one card in the deck)
+        throw new Error("Sorry but there are no more remaining cards");
+      }
+
+      let card = cardRes.data.cards[0];
+      this.setState(st => ({
+        drawn: [
+          ...st.drawn,
+          {
+            id: card.code,
+            image: card.image,
+            name: `${card.value} of ${card.suit}`
+          }
+        ]
+      }));
+    } catch (err) {
+      alert(err);
+    }
   }
+
   render() {
+    const cards = this.state.drawn.map(eachCard => (
+      <Card key={eachCard.id} name={eachCard.name} image={eachCard.image} />
+    ));
     return (
-      <div>
-        <h2>Card Dealer</h2>
+      <div className="Deck">
+        <h2 className="Deck-title">Card Dealer</h2>
+        <h5 className="Deck-title subtitle">
+          Just a little demo using react...
+        </h5>
         <button onClick={this.getCard}>Get Card</button>
+        <div className="Deck-cardarea">{cards}</div>
       </div>
     );
   }
